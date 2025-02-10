923fb360d31d68bee0a07b70d78156abc49f0526
harireddy7 <harimsd8@gmail.com>
2025-02-10 13:51:25 +0530

    feat: add ci cd pipeline with github actions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Learn Terraform (v0.0.1)</h1>
+        <h1>Learn Terraform ({process.env.REACT_APP_VERSION})</h1>
       </header>
       <hr />
       <main>
