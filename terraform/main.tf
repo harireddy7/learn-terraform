e658ffc86f86200b20c728920620edd14e569cd0
harireddy7 <harimsd8@gmail.com>
2025-02-11 12:26:18 +0530

    fix: use input variable for docker image path

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-# Provider
+# PROVIDERS
 
 provider "google" {
   project = "learn-terraform-450109"
@@ -6,13 +6,13 @@ provider "google" {
 }
 
 resource "google_cloud_run_service" "default" {
-  name     = "release"
+  name     = "release2"
   location = "asia-south1"
 
   template {
     spec {
       containers {
-        image = "asia-south1-docker.pkg.dev/learn-terraform-450109/arsenal/release:0.0.1"
+        image = var.image_path
         ports {
           container_port = 8080
         }
@@ -26,6 +26,8 @@ resource "google_cloud_run_service" "default" {
   }
 }
 
+# RESOURCES
+
 resource "google_cloud_run_service_iam_policy" "noauth" {
   location = google_cloud_run_service.default.location
   service  = google_cloud_run_service.default.name
@@ -38,4 +40,12 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
       }
     ]
   })
+}
+
+# VARIABLES
+
+variable "image_path" {
+  description = "Docker release image"
+  type = string
+  default = "asia-south1-docker.pkg.dev/learn-terraform-450109/arsenal/release:latest"
 }
